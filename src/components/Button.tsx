5914c813baaad621db68de5be71e9ad681dd4878
Daniel Zegarra <dzegarra@gmail.com>
2024-10-27 22:59:01 +0100

    Making all buttons red

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 
 export const Button = ({className, ...props}: ComponentProps<'button'>) => (
   <button
-    className={clsx("bg-sky-800 text-white px-3 py-2 rounded-md text-3xl disabled:bg-slate-400 disabled:cursor-not-allowed", className)}
+    className={clsx("bg-red-400 text-white px-3 py-2 rounded-md text-3xl disabled:bg-red-400 disabled:cursor-not-allowed", className)}
     {...props}
   />
 );
